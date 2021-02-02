3bd36b9e4f75f8d8a5a0ca225fc3fdf09ce9dbba
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-02-02 15:58:54 +0600

    Update PRoject2.java

--- PRoject2.java
+++ PRoject2.java
@@ -4,11 +4,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 import net.proteanit.sql.DbUtils;
 import javax.swing.border.EtchedBorder;
+
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -52,6 +52,7 @@ public class PRoject2 {
 	      catch (SQLException e) 
 	      {
 	      e.printStackTrace();
+		      
 	   } 
 	     }
 	/**
@@ -67,6 +68,7 @@ Connection con;
  ResultSet rs;
  private JTextField txtWelcomeToOur;
  private JTextField textpname;	
+	
 	public void Connect()
     {
         try {
@@ -83,6 +85,7 @@ Connection con;
         }
 
     }	
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -92,6 +95,7 @@ Connection con;
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);	
 		JPanel panel = new JPanel();
+		
 		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Regis", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.setBounds(10, 58, 209, 135);
 		frame.getContentPane().add(panel);
@@ -105,12 +109,14 @@ Connection con;
 		lblNewLabel_1_1.setBounds(10, 52, 70, 14);
 		panel.add(lblNewLabel_1_1);
 		JLabel lblNewLabel_1_2 = new JLabel("Age");
+		
 		lblNewLabel_1_2.setForeground(Color.RED);
 		lblNewLabel_1_2.setBounds(10, 72, 46, 14);
 		panel.add(lblNewLabel_1_2);
 		txtpname = new JTextField();
 		txtpname.setBounds(100, 24, 86, 20);
 		panel.add(txtpname);
+		
 		txtpname.setColumns(10);
 		txtvaccines = new JTextField();
 		txtvaccines.setBounds(100, 49, 86, 20);
@@ -122,6 +128,7 @@ Connection con;
 		panel.add(txtage);
 		txtage.setColumns(10);
 		JButton btnNewButton = new JButton("Save");
+		
 		btnNewButton.setForeground(Color.RED);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -142,6 +149,7 @@ Connection con;
 				 txtvaccines.setText("");
 				 txtage.setText("");
 				 txtpname.requestFocus();
+					 
 				    }
 				 
 				 catch (SQLException e1) 
@@ -166,6 +174,7 @@ Connection con;
 		btnClear.setBounds(162, 204, 68, 23);
 		frame.getContentPane().add(btnClear);
 		JButton btnExit = new JButton("Exit");
+		
 		btnExit.setForeground(Color.BLUE);
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {	
@@ -184,6 +193,7 @@ Connection con;
 		panel_1.setBounds(32, 252, 190, 57);
 		frame.getContentPane().add(panel_1);
 		JLabel lblNewLabel_1_1_1 = new JLabel("Id");
+		
 		lblNewLabel_1_1_1.setForeground(Color.RED);
 		panel_1.add(lblNewLabel_1_1_1);
 		txtpid = new JTextField();
@@ -207,6 +217,7 @@ try {
              }   
              else
              {
+		     
               txtpname.setText("");
               txtvaccines.setText("");
                  txtage.setText("");
@@ -234,6 +245,7 @@ try {
 				 pst.setString(3, agee);
 				 pst.setString(4, pid);
 				 pst.executeUpdate();
+					 
 				 JOptionPane.showMessageDialog(null, "Record is updated.");
 				 table_load();            
 				 txtpname.setText("");
@@ -250,6 +262,7 @@ try {
 		btnUpdate.setBounds(240, 204, 97, 23);
 		frame.getContentPane().add(btnUpdate);
 		JButton btnDelete = new JButton("Delete");
+		
 		btnDelete.setForeground(Color.RED);
 		btnDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -266,6 +279,7 @@ try {
 				 txtage.setText("");
 				 txtpname.requestFocus();
 				    }
+				
 				 catch (SQLException e1) 
 				        {
 				 e1.printStackTrace();
@@ -287,6 +301,7 @@ try {
 		frame.getContentPane().add(panel_1_1);
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("name");
 		lblNewLabel_1_1_1_1.setForeground(Color.RED);
+		
 		panel_1_1.add(lblNewLabel_1_1_1_1);
 		textpname = new JTextField();
 		textpname.addKeyListener(new KeyAdapter() {
@@ -299,6 +314,7 @@ try {
                  ResultSet rs = pst.executeQuery();
              if(rs.next()==true)
              {
+		     
                  String id = rs.getString(1);
                  String vac = rs.getString(2);
                  String agee = rs.getString(3);
